2d19288c25da7335ba9cfc5fdc526507a78f26e4
jan <jkaltenegger@ggc.edu>
2025-01-31 20:52:45 -0500

    Implemented reading local json files

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname)))
+app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
-const gitLoginRouter = require('./src/controllers/github-login')
+const gitLoginRouter = require('./src/routes/github-login')
 app.use('/', gitLoginRouter)
 
 app.listen(3000)
